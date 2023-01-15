e1b1bc95b5dbf75d796c4f51b434fe1cf589a092
notreplicated <71929133+notreplicated@users.noreply.github.com>
2023-01-15 01:54:25 +0100

    Create SteepStepsAntiTeleportBypass.lua

---
+++ SteepStepsAntiTeleportBypass.lua
@@ -0,0 +1,34 @@
+--[[
+
+╋╋╋╋╋╋┏┓╋╋╋╋╋╋╋╋┏┓╋╋╋╋╋╋╋┏┓╋╋╋╋╋┏┓
+╋╋╋╋╋┏┛┗┓╋╋╋╋╋╋╋┃┃╋╋╋╋╋╋┏┛┗┓╋╋╋╋┃┃
+┏━┓┏━┻┓┏╋━┳━━┳━━┫┃┏┳━━┳━┻┓┏╋━━┳━┛┃
+┃┏┓┫┏┓┃┃┃┏┫┃━┫┏┓┃┃┣┫┏━┫┏┓┃┃┃┃━┫┏┓┃
+┃┃┃┃┗┛┃┗┫┃┃┃━┫┗┛┃┗┫┃┗━┫┏┓┃┗┫┃━┫┗┛┃
+┗┛┗┻━━┻━┻┛┗━━┫┏━┻━┻┻━━┻┛┗┻━┻━━┻━━┛
+╋╋╋╋╋╋╋╋╋╋╋╋╋┃┃
+╋╋╋╋╋╋╋╋╋╋╋╋╋┗┛
+
+]]--
+
+--[[ 
+
+Anti teleport bypass for Steep Steps
+
+i think you would need to execute after my anti cheat bypass, (didnt test so not sure)
+
+ez ez ez ez ez ez
+
+]]--
+
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+
+local RootPart = Character:FindFirstChild("HumanoidRootPart")
+
+local CFrameLocation = CFrame.new(0, 0, 0) -- Or: Players:FindFirstChild("plr username").Character.Head.CFrame
+
+RootPart.Parent = nil
+RootPart.CFrame = CF
+RootPart.Parent = Character
