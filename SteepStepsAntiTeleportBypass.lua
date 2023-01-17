ec609f2d498cd33da4f384506665ab3f52b6fb49
notreplicated <71929133+notreplicated@users.noreply.github.com>
2023-01-17 10:34:47 +0100

    Update SteepStepsAntiTeleportBypass.lua

--- SteepStepsAntiTeleportBypass.lua
+++ SteepStepsAntiTeleportBypass.lua
@@ -13,35 +13,39 @@
 
 --[[ 
 
-Anti teleport bypass for Steep Steps
+Anti Teleport Bypass for Steep Steps
 
-i think you would need to execute after my anti cheat bypass, (didnt test so not sure)
+!! DO NOT PUT IN AUTOEXE !!
+!! EXECUTE THIS WHEN YOU ARE FULLY LOADED OUT OF THE BLACK BOX SPAWN AREA !!
 
-ez ez ez ez ez ez
+If there is any issues with the script not doing anything, try changing the "CFrame.new(0, -499, 0)" at line 28 into "CFrame.new(0, -500, 0)", same with line 43.
 
 ]]--
 
+repeat wait() until game:IsLoaded()
+
 local CoreGui = game:GetService("StarterGui")
-local Players = game.Players
-local LocalPlayer = game.Players.LocalPlayer
+local Players = game:GetService("Players")
+
+local LocalPlayer = Players.LocalPlayer
 
 CoreGui:SetCore("SendNotification", {Title = "Notification", Text = "Attempting to bypass Anti Teleport, please wait!", Duration = 3})
 
 Root = LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-Root.CFrame = CFrame.new(0, -500, 0)
+Root.CFrame = CFrame.new(0, -499, 0)
 
 LocalPlayer.Character.Humanoid.Died:Connect(function()
-	warn("died, now waiting 3 seconds before teleporting")
-	task.wait(4)
-	LocalPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame = CFrame.new(-738, 2499, -308)
-	
-	Root = LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-	Root.CFrame = CFrame.new(0, -500, 0)
-		
-	LocalPlayer.Character.Humanoid.Died:Connect(function()
-		warn("died, now waiting 3 seconds before teleporting again LOL")
-		task.wait(4)
-		LocalPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame = CFrame.new(-738, 2499, -308)
-		CoreGui:SetCore("SendNotification", {Title = "Notification", Text = "Anti Teleport has been successfully bypassed! Feel free to teleport around now.", Duration = 5})
-	end)
+    warn("died, now waiting 3 seconds before teleporting")
+    task.wait(4)
+    LocalPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame = CFrame.new(-738, 2499, -308)
+    
+    Root = LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+    Root.CFrame = CFrame.new(0, -499, 0)
+        
+    LocalPlayer.Character.Humanoid.Died:Connect(function()
+        warn("died, now waiting 3 seconds before teleporting again LOL")
+        task.wait(4)
+        LocalPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame = CFrame.new(-738, 2499, -308)
+        CoreGui:SetCore("SendNotification", {Title = "Notification", Text = "Anti Teleport has been successfully bypassed! Feel free to teleport around now.", Duration = 5})
+    end)
 end)
